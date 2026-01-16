af9c5f9c8080df6e82c0ff02e2f9be8833b51124
gautam-4 <gautamalt468@gmail.com>
2026-01-16 16:30:36 +0530

    update eta logic

--- utils/estimateTime.js
+++ utils/estimateTime.js
@@ -1,7 +1,7 @@
 export const calculateEstimatedTime = (itemCount) => {
-  // Base time of 15 minutes + 5 minutes per item, with a max of 45 minutes
+  // Base time of 15 minutes + 1 minutes per item, with a max of 45 minutes
   const baseTime = 15;
-  const timePerItem = 5;
+  const timePerItem = 1;
   const maxTime = 45;
   const minTime = 10;
 
